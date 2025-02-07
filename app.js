a16e8839aeec4fff7f2e6e38b16fa561b6af545b
Pedro Benegas <pedrophbs8199@gmail.com>
2025-02-06 23:54:53 -0300

    add: middlewares

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ import path from "path";
 import cors from "cors";
 import "dotenv/config";
 import { router } from "./routes/Router.js";
+import { db } from "./config/db.js"
 
 const app = express();
 // config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+// solve cors
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+// upload directory
+app.use("/uploads", express.static(path.join(import.meta.dirname, "uploads")));
+// DB connection
+
+// routes
 app.use(router);
 
 app.listen(process.env.PORT, () => {
